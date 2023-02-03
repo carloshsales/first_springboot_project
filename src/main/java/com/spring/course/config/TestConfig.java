b834a8d9104b0d2a8357e5fff8ea31845b3a2e60
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-03 12:15:01 -0300

    feat: status code enumerator and implementation

--- src/main/java/com/spring/course/config/TestConfig.java
+++ src/main/java/com/spring/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.spring.course.config;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.spring.course.entities.Order;
 import com.spring.course.entities.User;
+import com.spring.course.entities.enums.OrderStatus;
+import com.spring.course.repositories.OrderRepository;
 import com.spring.course.repositories.UserRepository;
 
 @Configuration
@@ -17,6 +21,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -24,11 +31,20 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Jupiter", "jupiter@teste.com", "74999999999", "juju");
 		User u2 = new User(null, "Pandora", "pandora@teste.com", "74999999999", "dora");
 		
-		List<User> list = new ArrayList<>();
-		list.add(u1);
-		list.add(u2);
-	
-		userRepository.saveAll(list);
+		Order o1 = new Order(null, Instant.parse("2023-02-02T22:59:00Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2023-02-02T22:59:00Z"), OrderStatus.PAID, u2);
+		
+		List<User> listU = new ArrayList<>();
+		listU.add(u1);
+		listU.add(u2);
+
+		userRepository.saveAll(listU);
+		
+		List<Order> listO = new ArrayList<>();
+		listO.add(o1);
+		listO.add(o2);
+
+		orderRepository.saveAll(listO);
 	}
 	
 	
---
+++ src/main/java/com/spring/course/resources/OrderResource.java
@@ -0,0 +1,43 @@
+package com.spring.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.course.entities.Order;
+import com.spring.course.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+	
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Order>> getOrders(){
+		List<Order> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> getById(@PathVariable Long id){
+		Order order = service.findById(id);
+		return ResponseEntity.ok().body(order);
+	}
+}
+
+
+
+
+
+
+
+
+
+
