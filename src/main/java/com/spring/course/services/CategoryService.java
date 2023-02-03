395555848d0ad80aabfd592d22eef7d331c9d2d6
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-03 16:39:02 -0300

    feat: category services

---
+++ src/main/java/com/spring/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.spring.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.course.entities.Category;
+import com.spring.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> category = categoryRepository.findById(id);
+		return category.get();
+	}
+}
