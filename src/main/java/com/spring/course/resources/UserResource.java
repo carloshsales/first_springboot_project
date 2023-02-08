e5a37364082f278e622e670c6ff9985d669151e0
Carlos Henrique <carlosh.sales@outlook.com>
2023-02-07 23:44:21 -0300

    feat: user resource update - controller

--- src/main/java/com/spring/course/resources/UserResource.java
+++ src/main/java/com/spring/course/resources/UserResource.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +53,12 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id,@RequestBody User user){
+		user = service.update(id, user);
+		return ResponseEntity.ok().body(user);
 	}
 }
 
